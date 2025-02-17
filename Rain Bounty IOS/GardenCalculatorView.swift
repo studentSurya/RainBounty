4e749104ac9010a35d591c85e8cabd8b905e2241
studentSurya <surya@outlook.com>
2025-02-17 13:55:46 -0600

    Refactored out GardenCalculator into a separate file
    Refactored out GardenCalculator into a separate file

---
+++ Rain Bounty IOS/GardenCalculatorView.swift
@@ -0,0 +1,81 @@
+//
+//  GardenCalculatorView.swift
+//  Rain Bounty IOS
+//
+//  Created by Surya Swaminathan on 2/17/25.
+//
+
+import SwiftUI
+
+struct GardenCalculatorView: View {
+    @AppStorage("uiStateGardenSizeToggleOn") var uiStateGardenSizeToggleOn: Bool = false
+    @AppStorage("uiStateGardenSizeStr") var uiStateGardenSizeStr: String = ""
+    @AppStorage("uiStateGardenLength") var uiStateGardenLength: String = ""
+    @AppStorage("uiStateGardenWidth") var uiStateGardenWidth: String = ""
+    
+    @AppStorage("gardenAreaInt") var gardenAreaInt: Int = 0
+    
+    var body: some View {
+        Form{
+            Section (header: Text("Garden Size"), footer: Text("This data allows us to estimate your garden/lawn irrigation needs based of your garden/lawn area."))
+            {
+               // Toggle(isOn: $uiStateGardenSizeToggleOn, label: {Text("Enter Garden Area directly")})
+               //     .onChange(of: uiStateGardenSizeToggleOn) {
+               //         computeGardenArea()
+               //     }
+                
+                Picker("Garden area options?", selection: $uiStateGardenSizeToggleOn)
+                {
+                    Text("Direct").tag(true)
+                    Text("L x W ").tag(false)
+                }
+                .pickerStyle(.segmented)
+                .onChange(of: uiStateGardenSizeToggleOn) {
+                     computeGardenArea()
+                }
+                
+                if(uiStateGardenSizeToggleOn)
+                {
+                    TextField("Enter your garden size here (sqft)", text: $uiStateGardenSizeStr)
+                       .keyboardType(.numberPad)
+                       .textFieldStyle(.roundedBorder)
+                       .onChange(of: uiStateGardenSizeStr) {
+                           computeGardenArea()
+                       }
+                }
+                else {
+                    TextField("Enter your house length here (ft)", text: $uiStateGardenLength)
+                       .keyboardType(.numberPad)
+                       .textFieldStyle(.roundedBorder)
+                       .onChange(of: uiStateGardenLength) {
+                           computeGardenArea()
+                       }
+                    
+                    TextField("Enter your house width here (ft)", text: $uiStateGardenWidth)
+                       .keyboardType(.numberPad)
+                       .textFieldStyle(.roundedBorder)
+                       .onChange(of: uiStateGardenWidth) {
+                           computeGardenArea()
+                       }
+                    
+                    
+                    Text ("Garden area (sqft): \(gardenAreaInt)")
+                }
+
+                
+            }
+        
+        }
+        .navigationTitle(Text("Garden area calculator"))
+        
+    }
+    
+    func computeGardenArea() {
+        if(uiStateGardenSizeToggleOn) {
+            gardenAreaInt = Int(uiStateGardenSizeStr) ?? 0
+        }
+        else {
+            gardenAreaInt = (Int(uiStateGardenLength) ?? 0) * (Int(uiStateGardenWidth) ?? 0 )
+        }
+    }
+}
